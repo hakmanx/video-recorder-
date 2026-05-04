df1998b9d99e84882c5a856a0dbf96f456da33e2
hakmanx <hakmanx@users.noreply.github.com>
2026-05-04 12:38:21 +0000

    Add concurrent futures dependency for CameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.lifecycle:lifecycle-service:2.8.7")
+    implementation("androidx.concurrent:concurrent-futures:1.3.0")
 
     implementation("androidx.camera:camera-core:1.6.0")
     implementation("androidx.camera:camera-camera2:1.6.0")
